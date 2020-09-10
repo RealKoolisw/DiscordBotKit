3329b900f664a2f757ec6e7e645b3daf75dc3b1b
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2020-09-10 16:46:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 client.login(config.TOKEN);
 client.config = config;
 const http = require("http");
-http.createServer((_, res) => res.end("Alive")).listen(8080)
+http.createServer((_, res) => res.end("如果您觀看此消息，則表示您的機器人已成功運行，並將網址添加到uptimerobot")).listen(8080)
 
 client.on("ready", () => {
   console.log("The bot has started!");
